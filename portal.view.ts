e4055a61e266cd2d36d55a4865b82a37dda4e74f
jin <jin@hyoo.ru>
2021-06-04 20:16:07 +0300

    Initial impl

---
+++ portal.view.ts
@@ -0,0 +1,41 @@
+namespace $.$$ {
+
+	export class $mdh_portal extends $.$mdh_portal {
+
+		app() {
+			return this.$.$mol_state_arg.value( 'app' )
+		}
+		
+		@ $mol_mem
+		menu_items() {
+			return Object.keys( this.data() ).map( app => this.Menu_item( app ) )
+		}
+		
+		pages() {
+			const app = this.app()
+			return [
+				this.Menu(),
+				... app ? [ this.App( app ) ] : [],
+			]
+		}
+		
+		app_title( app: string ) {
+			return this.data()[ app ].title
+		}
+
+		@ $mol_mem_key
+		app_uri( app: string, next?: string ) {
+			if( this.app() === app ) {
+				return this.$.$mol_state_arg.value( 'uri', next ) ?? this.data()[ app ].uri
+			} else {
+				return this.data()[ app ].uri
+			}
+		}
+
+		app_arg( app: string ) {
+			return { app, uri: null }
+		}
+
+	}
+
+}
